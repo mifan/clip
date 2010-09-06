5a9bbb070337045b761752b07554a9182e346db5
mifan <mifanxp@gmail.com>
2010-09-06 22:15:53 +0800

    skeleton of rails 3 GA

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Clip::Application.config.secret_token = '998ae1ff183369f1ae1a74124eed84984b997eb220b285fcb0247d283fa77432c7e1ac5a79b96d0986ec70dd2a88e4eededb2fe6212b97c07865a39bd9998b8b'
